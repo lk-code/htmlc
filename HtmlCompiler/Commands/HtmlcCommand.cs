d986f326e09d53ba42fb742d43359d1f011a4f39
Lars <lars.kraemer@live.de>
2022-08-21 17:36:22 +0200

    update

---
+++ HtmlCompiler/Commands/HtmlcCommand.cs
@@ -0,0 +1,34 @@
+﻿using Cocona;
+using HtmlCompiler.Core.Extensions;
+using HtmlCompiler.Core.Interfaces;
+
+namespace HtmlCompiler.Commands
+{
+    public class HtmlcCommand
+    {
+        private readonly IHtmlRenderer _htmlRenderer;
+
+        public HtmlcCommand(IHtmlRenderer htmlRenderer)
+        {
+            this._htmlRenderer = htmlRenderer ?? throw new ArgumentNullException(nameof(htmlRenderer));
+        }
+
+        [Command("compile")]
+        public async Task Compile([Argument(Description = "path to the source file")] string sourceFile,
+            [Argument(Description = "path to the output file")] string outputFile)
+        {
+            string fullOutputFilePath = outputFile.FromSourceFilePath(sourceFile);
+
+            Console.WriteLine($"compile file {sourceFile} to {fullOutputFilePath}");
+
+            try
+            {
+                await this._htmlRenderer.RenderToFileAsync(sourceFile, fullOutputFilePath);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine($"file {sourceFile} not found");
+            }
+        }
+    }
+}
--- HtmlCompiler/Program.cs
+++ HtmlCompiler/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Cocona;
 using Cocona.Builder;
+using HtmlCompiler.Commands;
 using HtmlCompiler.Core;
-using HtmlCompiler.Core.Extensions;
 using HtmlCompiler.Core.Interfaces;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
@@ -11,23 +11,6 @@ builder.Services.TryAddSingleton<IHtmlRenderer, HtmlRenderer>();
 
 CoconaApp? app = builder.Build();
 
-app.AddCommand(async (
-    [Argument(Description = "path to the source file")] string sourceFile,
-    [Argument(Description = "path to the output file")] string outputFile,
-    IHtmlRenderer htmlRenderer) =>
-{
-    string fullOutputFilePath = outputFile.FromSourceFilePath(sourceFile);
-
-    Console.WriteLine($"compile file {sourceFile} to {fullOutputFilePath}");
-
-    try
-    {
-        await htmlRenderer.RenderToFileAsync(sourceFile, fullOutputFilePath);
-    }
-    catch (FileNotFoundException)
-    {
-        Console.WriteLine($"file {sourceFile} not found");
-    }
-});
+app.AddCommands<HtmlcCommand>();
 
 app.Run();
