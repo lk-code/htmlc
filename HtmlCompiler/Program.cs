30b5566c5f2f865c1a51be86e9766409197832b7
Lars Krämer <lars.kraemer@live.de>
2023-01-04 23:00:44 +0100

    HTMLC-25: added appsettings and user config file

--- HtmlCompiler/Program.cs
+++ HtmlCompiler/Program.cs
@@ -3,10 +3,27 @@ using Cocona.Builder;
 using HtmlCompiler.Commands;
 using HtmlCompiler.Core;
 using HtmlCompiler.Core.Interfaces;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 
 CoconaAppBuilder? builder = CoconaApp.CreateBuilder();
+
+// user config file
+string userConfigFileFullPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".htmlc");
+
+// if not exists => create
+if (!File.Exists(userConfigFileFullPath))
+{
+    using (StreamWriter sw = File.CreateText(userConfigFileFullPath))
+    {
+        sw.WriteLine("{}");
+    }
+    File.SetAttributes(userConfigFileFullPath, FileAttributes.Hidden);
+}
+
+builder.Configuration
+    .AddJsonFile("appsettings.json", false, false)
+    .AddJsonStream(new StreamReader(userConfigFileFullPath).BaseStream);
 
 builder.Services.AddSingleton<IHtmlRenderer, HtmlRenderer>();
 builder.Services.AddTransient<IHtmlWatcher, HtmlWatcher>();
