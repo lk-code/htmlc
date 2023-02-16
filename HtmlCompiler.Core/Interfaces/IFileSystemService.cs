48558362ffacb65afc92e5a990c133f37619f880
Lars Krämer <lars.kraemer@live.de>
2023-02-16 20:01:23 +0100

    HTMLC-58: fixed interface

--- HtmlCompiler.Core/Interfaces/IFileSystemService.cs
+++ HtmlCompiler.Core/Interfaces/IFileSystemService.cs
@@ -6,3 +6,4 @@ public interface IFileSystemService
     Task<string> FileReadAllTextAsync(string path, CancellationToken cancellationToken = default(CancellationToken));
     Task FileWriteAllTextAsync(string path, string? contents, CancellationToken cancellationToken = default(CancellationToken));
     void FileCopy(string sourceFileName, string destFileName, bool overwrite);
+}
