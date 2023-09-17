19b3f6f1df9475701d2ca80e351ffa3d104de7db
Lars Krämer <lars.kraemer@live.de>
2023-09-17 16:57:31 +0200

    HTMLC-101: added additional tests

--- HtmlCompiler.Tests/Core/HtmlRendererTests.cs
+++ HtmlCompiler.Tests/Core/HtmlRendererTests.cs
@@ -18,7 +18,7 @@ public class HtmlRendererTests
     public void SetUp()
     {
         this._fileSystemService = Substitute.For<IFileSystemService>();
-        
+
         this._instance = new HtmlRenderer(this._fileSystemService);
     }
 
@@ -29,18 +29,18 @@ public class HtmlRendererTests
         string sourceDirectory = "/project/src";
         string outputDirectory = "/project/dist";
         string? cssOutputFilePath = null;
-        
+
         string expectedHtml = "Hello World!";
 
         this._fileSystemService.FileReadAllTextAsync($"{sourceDirectory}/index.html")
             .Returns("Hello World!");
-        
+
         string result = await this._instance.RenderHtmlAsync(sourceFullFilePath,
             sourceDirectory,
             outputDirectory,
             cssOutputFilePath,
             null);
-        
+
         result.Should().NotBeNullOrEmpty();
         result.Should().Be(expectedHtml);
     }
@@ -52,7 +52,7 @@ public class HtmlRendererTests
         string sourceDirectory = "/project/src";
         string outputDirectory = "/project/dist";
         string? cssOutputFilePath = null;
-        
+
         var expectedHtml = new StringBuilder()
             .AppendLine("<html>")
             .AppendLine("<head>")
@@ -63,7 +63,7 @@ public class HtmlRendererTests
             .AppendLine("</body>")
             .AppendLine("</html>")
             .ToString().Trim();
-        
+
         var layoutHtml = new StringBuilder()
             .AppendLine("<html>")
             .AppendLine("<body>")
@@ -81,13 +81,13 @@ public class HtmlRendererTests
             .Returns(contentHtml);
         this._fileSystemService.FileReadAllTextAsync($"{sourceDirectory}/_layoutbase.html")
             .Returns(layoutHtml);
-        
+
         string result = await this._instance.RenderHtmlAsync(sourceFullFilePath,
             sourceDirectory,
             outputDirectory,
             cssOutputFilePath,
             null);
-        
+
         result.Should().NotBeNullOrEmpty();
         result.Should().Be(expectedHtml);
     }
@@ -99,7 +99,7 @@ public class HtmlRendererTests
         string sourceDirectory = "/project/src";
         string outputDirectory = "/project/dist";
         string? cssOutputFilePath = null;
-        
+
         var expectedHtml = new StringBuilder()
             .AppendLine("<html>")
             .AppendLine("<head>")
@@ -119,7 +119,7 @@ public class HtmlRendererTests
             .ToString().Trim();
         this._fileSystemService.FileReadAllTextAsync($"{sourceDirectory}/index.html")
             .Returns(indexContent);
-        
+
         var layoutContent = new StringBuilder()
             .AppendLine("<html>")
             .AppendLine("<head>")
@@ -132,13 +132,13 @@ public class HtmlRendererTests
             .ToString().Trim();
         this._fileSystemService.FileReadAllTextAsync($"{sourceDirectory}/_layoutbase.html")
             .Returns(layoutContent);
-        
+
         string result = await this._instance.RenderHtmlAsync(sourceFullFilePath,
             sourceDirectory,
             outputDirectory,
             cssOutputFilePath,
             null);
-        
+
         result.Should().NotBeNullOrEmpty();
         result.Should().Be(expectedHtml);
     }
@@ -150,7 +150,7 @@ public class HtmlRendererTests
         string sourceDirectory = "/project/src";
         string outputDirectory = "/project/dist";
         string? cssOutputFilePath = "/css/site.scss";
-        
+
         var expectedHtml = new StringBuilder()
             .AppendLine("<html>")
             .AppendLine("<head>")
@@ -171,7 +171,7 @@ public class HtmlRendererTests
             .ToString().Trim();
         this._fileSystemService.FileReadAllTextAsync($"{sourceDirectory}/index.html")
             .Returns(indexContent);
-        
+
         var layoutContent = new StringBuilder()
             .AppendLine("<html>")
             .AppendLine("<head>")
@@ -185,13 +185,13 @@ public class HtmlRendererTests
             .ToString().Trim();
         this._fileSystemService.FileReadAllTextAsync($"{sourceDirectory}/_layoutbase.html")
             .Returns(layoutContent);
-        
+
         string result = await this._instance.RenderHtmlAsync(sourceFullFilePath,
             sourceDirectory,
             outputDirectory,
             cssOutputFilePath,
             null);
-        
+
         result.Should().NotBeNullOrEmpty();
         result.Should().Be(expectedHtml);
     }
@@ -207,7 +207,7 @@ public class HtmlRendererTests
             .Add("Application", new DataBuilder()
                 .Add("Name", "htmlc demo")
                 .Add("Version", "v1.0.7"));
-        
+
         var expectedHtml = new StringBuilder()
             .AppendLine("<html>")
             .AppendLine("<head>")
@@ -228,7 +228,7 @@ public class HtmlRendererTests
             .ToString().Trim();
         this._fileSystemService.FileReadAllTextAsync($"{sourceDirectory}/index.html")
             .Returns(indexContent);
-        
+
         var layoutContent = new StringBuilder()
             .AppendLine("<html>")
             .AppendLine("<head>")
@@ -242,13 +242,13 @@ public class HtmlRendererTests
             .ToString().Trim();
         this._fileSystemService.FileReadAllTextAsync($"{sourceDirectory}/_layoutbase.html")
             .Returns(layoutContent);
-        
+
         string result = await this._instance.RenderHtmlAsync(sourceFullFilePath,
             sourceDirectory,
             outputDirectory,
             cssOutputFilePath,
             dataBuilder.Build().RootElement);
-        
+
         result.Should().NotBeNullOrEmpty();
         result.Should().Be(expectedHtml);
     }
@@ -264,7 +264,7 @@ public class HtmlRendererTests
             .Add("Application", new DataBuilder()
                 .Add("Name", "htmlc demo")
                 .Add("Version", "v1.0.7"));
-        
+
         var expectedHtml = new StringBuilder()
             .AppendLine("<html>")
             .AppendLine("<head>")
@@ -308,7 +308,7 @@ public class HtmlRendererTests
             .Returns(true);
         this._fileSystemService.FileReadAllTextAsync($"{sourceDirectory}/README.md")
             .Returns(markdownContent);
-        
+
         var layoutContent = new StringBuilder()
             .AppendLine("<html>")
             .AppendLine("<head>")
@@ -322,13 +322,13 @@ public class HtmlRendererTests
             .ToString().Trim();
         this._fileSystemService.FileReadAllTextAsync($"{sourceDirectory}/_layoutbase.html")
             .Returns(layoutContent);
-        
+
         string result = await this._instance.RenderHtmlAsync(sourceFullFilePath,
             sourceDirectory,
             outputDirectory,
             cssOutputFilePath,
             dataBuilder.Build().RootElement);
-        
+
         result.Should().NotBeNullOrEmpty();
         result.Should().Be(expectedHtml);
     }
@@ -344,7 +344,7 @@ public class HtmlRendererTests
             .Add("Application", new DataBuilder()
                 .Add("Name", "htmlc demo")
                 .Add("Version", "v1.0.7"));
-        
+
         var expectedHtml = new StringBuilder()
             .AppendLine("<html>")
             .AppendLine("<head>")
@@ -382,7 +382,7 @@ public class HtmlRendererTests
             .Returns(true);
         this._fileSystemService.FileReadAllTextAsync($"{sourceDirectory}/section.html")
             .Returns(sectionContent);
-        
+
         var layoutContent = new StringBuilder()
             .AppendLine("<html>")
             .AppendLine("<head>")
@@ -396,13 +396,13 @@ public class HtmlRendererTests
             .ToString().Trim();
         this._fileSystemService.FileReadAllTextAsync($"{sourceDirectory}/_layoutbase.html")
             .Returns(layoutContent);
-        
+
         string result = await this._instance.RenderHtmlAsync(sourceFullFilePath,
             sourceDirectory,
             outputDirectory,
             cssOutputFilePath,
             dataBuilder.Build().RootElement);
-        
+
         result.Should().NotBeNullOrEmpty();
         result.Should().Be(expectedHtml);
     }
@@ -418,7 +418,7 @@ public class HtmlRendererTests
             .Add("Application", new DataBuilder()
                 .Add("Name", "htmlc demo")
                 .Add("Version", "v1.0.7"));
-        
+
         var expectedHtml = new StringBuilder()
             .AppendLine("<html>")
             .AppendLine("<head>")
@@ -459,7 +459,7 @@ public class HtmlRendererTests
             .Returns(true);
         this._fileSystemService.FileReadAllTextAsync($"{sourceDirectory}/section.html")
             .Returns(sectionContent);
-        
+
         var layoutContent = new StringBuilder()
             .AppendLine("<html>")
             .AppendLine("<head>")
@@ -473,13 +473,114 @@ public class HtmlRendererTests
             .ToString().Trim();
         this._fileSystemService.FileReadAllTextAsync($"{sourceDirectory}/_layoutbase.html")
             .Returns(layoutContent);
-        
+
         string result = await this._instance.RenderHtmlAsync(sourceFullFilePath,
             sourceDirectory,
             outputDirectory,
             cssOutputFilePath,
             dataBuilder.Build().RootElement);
-        
+
+        result.Should().NotBeNullOrEmpty();
+        result.Should().Be(expectedHtml);
+    }
+
+    [TestMethod]
+    public async Task RenderHtmlAsync_WithVariablesTagAndLargeMixedArrayAndObjects_Return()
+    {
+        string sourceFullFilePath = "/project/src/index.html";
+        string sourceDirectory = "/project/src";
+        string outputDirectory = "/project/dist";
+        string? cssOutputFilePath = null;
+        IDataBuilder dataBuilder = new DataBuilder()
+            .Add("Application", new DataBuilder()
+                .Add("Name", "htmlc demo"));
+
+        var expectedHtml = new StringBuilder()
+            .AppendLine("<html>")
+            .AppendLine("<head>")
+            .AppendLine("<title>htmlc demo</title>")
+            .AppendLine("<meta name=\"generator\" content=\"htmlc\">")
+            .AppendLine("</head>")
+            .AppendLine("<body>")
+            .AppendLine("<h1>Hello World!</h1>")
+            .AppendLine("<section>")
+            .AppendLine("<p>Section Page from htmlc</p>")
+            .AppendLine("</section>")
+            .AppendLine("<pre>v3.2.491</pre>")
+            .AppendLine("</body>")
+            .AppendLine("</html>")
+            .ToString().Trim();
+
+        var jsonContent = new DataBuilder()
+            .Add("Images", new DataBuilder()
+                .Add("Icons", new List<string>
+                {
+                    "appicon_small_64x.png",
+                    "appicon_medium_128x.png",
+                    "appicon_large_256x.png"
+                })
+                .Add("Header", new List<string>
+                {
+                    "default_1080x.jpg",
+                    "default_4k.jpg"
+                }))
+            .Add("CMS", new List<object>
+            {
+                new DataBuilder()
+                    .Add("Meta", new DataBuilder()
+                        .Add("Version", new DataBuilder()
+                            .Add("Major", 3)
+                            .Add("Minor", 2)
+                            .Add("Patch", 491)
+                        )
+                    )
+                    .GetProperties()
+            })
+            .Build().RootElement.GetRawText();
+
+        var indexContent = new StringBuilder()
+            .AppendLine("@Var={\"Title\":\"Hello World!\"}")
+            .AppendLine("@Var={\"Website\":{\"Author\":\"htmlc\"}}")
+            .AppendLine("@Var=" + jsonContent)
+            .AppendLine("@Layout=_layoutbase.html")
+            .AppendLine("@PageTitle=@Global:Application:Name")
+            .AppendLine("<h1>Hello World!</h1>")
+            .AppendLine("<section>")
+            .AppendLine("@File=section.html")
+            .AppendLine("</section>")
+            .ToString().Trim();
+        this._fileSystemService.FileReadAllTextAsync($"{sourceDirectory}/index.html")
+            .Returns(indexContent);
+
+        var sectionContent = new StringBuilder()
+            .AppendLine("@Var={\"Title\":\"Section Page\"}")
+            .AppendLine("<p>@Var[\"Title\"]; from @Var[\"Website:Author\"];</p>")
+            .ToString().Trim();
+        this._fileSystemService.FileExists($"{sourceDirectory}/section.html")
+            .Returns(true);
+        this._fileSystemService.FileReadAllTextAsync($"{sourceDirectory}/section.html")
+            .Returns(sectionContent);
+
+        var layoutContent = new StringBuilder()
+            .AppendLine("<html>")
+            .AppendLine("<head>")
+            .AppendLine("<title>@PageTitle</title>")
+            .AppendLine("</head>")
+            .AppendLine("<body>")
+            .AppendLine("@Body")
+            .AppendLine("<pre>v@Var[\"CMS:[0]:Meta:Version:Major\"];.@Var[\"CMS:[0]:Meta:Version:Minor\"];.@Var[\"CMS:[0]:Meta:Version:Patch\"];</pre>")
+            .AppendLine("</body>")
+            .AppendLine("</html>")
+            .ToString().Trim();
+        this._fileSystemService.FileReadAllTextAsync($"{sourceDirectory}/_layoutbase.html")
+            .Returns(layoutContent);
+
+        string result = await this._instance.RenderHtmlAsync(sourceFullFilePath,
+            sourceDirectory,
+            outputDirectory,
+            cssOutputFilePath,
+            dataBuilder.Build().RootElement);
+
         result.Should().NotBeNullOrEmpty();
         result.Should().Be(expectedHtml);
     }
