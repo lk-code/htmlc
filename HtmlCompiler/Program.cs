db57591b3b1db2f24e97de35d4895a46a17751c3
Lars Krämer <lars.kraemer@live.de>
2023-09-27 22:20:09 +0200

    HTMLC-104: fixed appsettings

--- HtmlCompiler/Program.cs
+++ HtmlCompiler/Program.cs
@@ -1,8 +1,10 @@
-﻿using System.Text.Json;
+﻿using System.Reflection;
+using System.Text.Json;
 using Cocona;
 using Cocona.Builder;
 using FluentDataBuilder;
 using FluentDataBuilder.Json;
+using FluentDataBuilder.Microsoft.Extensions.Configuration;
 using HtmlCompiler.Commands;
 using HtmlCompiler.Config;
 using HtmlCompiler.Core;
@@ -36,8 +38,15 @@ static class Program
         EnsureUserCacheDirectory(userCacheDirectoryPath);
 
         CoconaAppBuilder? builder = CoconaApp.CreateBuilder(args);
+        IDataBuilder configBuilder = new DataBuilder();
 
-        var logger = LoggerFactory.Create(builder => builder.AddNLog()).CreateLogger<ProgramLog>();
+        // load appsettings
+        using Stream appsettingsStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("HtmlCompiler.appsettings.json")!;
+        using StreamReader appsettingsReader = new(appsettingsStream);
+        string appsettingsJson = appsettingsReader.ReadToEnd();
+        configBuilder = DataBuilder.Merge(configBuilder, new DataBuilder().LoadFrom(appsettingsJson));
+
+        ILogger<ProgramLog> logger = LoggerFactory.Create(builder => builder.AddNLog()).CreateLogger<ProgramLog>();
         logger.LogTrace("##################################################");
         builder.Services.AddLogging(loggingBuilder =>
         {
@@ -46,16 +55,15 @@ static class Program
             loggingBuilder.SetMinimumLevel(LogLevel.Trace);
             loggingBuilder.AddNLog();
         });
-        
+
         string logFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "htmlc.log");
         logger.LogInformation($"Log-File: {logFilePath}");
 
         // add user configuration
-        // logger.LogTrace($"add user configuration file: '{userConfigPath}'");
-        // builder.Configuration.AddJsonStream(new StreamReader(userConfigPath).BaseStream);
+        logger.LogTrace($"add user configuration file: '{userConfigPath}'");
+        configBuilder = DataBuilder.Merge(configBuilder, new DataBuilder().LoadFrom(new StreamReader(userConfigPath).ReadToEnd()));
 
-        builder.Services.AddTransient<IConfigurationManager>(x =>
-            new Config.ConfigurationManager(userConfigPath, x.GetRequiredService<IFileSystemService>()));
+        builder.Services.AddTransient<IConfigurationManager>(x => new Config.ConfigurationManager(userConfigPath, x.GetRequiredService<IFileSystemService>()));
 
         // add services
         builder.Services.AddSingleton<IHtmlRenderer, HtmlRenderer>();
@@ -74,21 +82,18 @@ static class Program
         builder.Services.AddTransient<IDependencyObject, LessDependency>();
         builder.Services.AddTransient<IDependencyObject, NodeDependency>();
 
-        // add temp variables
-        IDataBuilder dataBuilder = new DataBuilder();
-        dataBuilder.Add("Core", new DataBuilder()
+        // add global core variables
+        configBuilder.Add("Core", new DataBuilder()
             .Add("UserConfigPath", userConfigPath)
             .Add("UserCacheDirectoryPath", userCacheDirectoryPath));
 
-        string jsonString = dataBuilder.Build().RootElement.GetRawText();
-        using MemoryStream coreConfigJsonStream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(jsonString));
-        builder.Configuration.AddJsonStream(coreConfigJsonStream);
+        // load FluentDataBuilder with appsettings and user-configuration to IConfiguration
+        builder.Configuration.AddConfiguration(configBuilder.ToConfiguration());
 
         CoconaApp? app = builder.Build();
-        
+
         // store final configuration for debug
         string finalConfiguration = (app.Configuration as Microsoft.Extensions.Configuration.ConfigurationManager)!.GetDebugView();
-        
         logger.LogTrace(finalConfiguration);
 
         app.AddCommands<HtmlcCommand>();
