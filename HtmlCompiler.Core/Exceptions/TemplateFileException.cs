5b795f1b2878c8c301a2f99bd539f43b835221f7
Lars Krämer <lars.kraemer@live.de>
2023-10-10 00:51:16 +0200

    HTMLC-127: removed unused exception ctor

--- HtmlCompiler.Core/Exceptions/TemplateFileException.cs
+++ HtmlCompiler.Core/Exceptions/TemplateFileException.cs
@@ -4,18 +4,6 @@ public class TemplateFileException : Exception
 {
     public IReadOnlyCollection<string> Errors { get; }
     
-    public TemplateFileException()
-    {
-    }
-
-    public TemplateFileException(string message) : base(message)
-    {
-    }
-
-    public TemplateFileException(string message, Exception innerException) : base(message, innerException)
-    {
-    }
-
     public TemplateFileException(IReadOnlyCollection<string> errors)
     {
         this.Errors = errors;
