9ba52566949e494e5b98ce89e271ebdedcfcee19
Lars Krämer <lars.kraemer@live.de>
2023-10-10 13:18:58 +0200

    HTMLC-127: added comments

--- HtmlCompiler.Core/Interfaces/ICLIManager.cs
+++ HtmlCompiler.Core/Interfaces/ICLIManager.cs
@@ -2,5 +2,10 @@ namespace HtmlCompiler.Core.Interfaces;
 
 public interface ICLIManager
 {
+    /// <summary>
+    /// executes the given command directly on the console
+    /// </summary>
+    /// <param name="command">the command to execute</param>
+    /// <returns></returns>
     string ExecuteCommand(string command);
 }
