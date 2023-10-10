64e0b6e7b9d2669b4364132316d2f9b8a96eb2f3
Lars Krämer <lars.kraemer@live.de>
2023-10-10 13:12:49 +0200

    HTMLC-127: added comments for datetimeprovider

--- HtmlCompiler.Core/Interfaces/IDateTimeProvider.cs
+++ HtmlCompiler.Core/Interfaces/IDateTimeProvider.cs
@@ -3,7 +3,7 @@ namespace HtmlCompiler.Core.Interfaces;
 public interface IDateTimeProvider
 {
     /// <summary>
-    /// 
+    /// Returns a DateTime representing the current date and time. The resolution of the returned value depends on the system timer.
     /// </summary>
     /// <returns></returns>
     DateTime Now();
